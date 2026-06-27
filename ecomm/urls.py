83e7d5ea0a2e9e26e1415c6d056c39ab36965937
manoj70356 <manojsingh805730@gmail.com>
2026-06-27 18:05:21 +0530

    Now all html page linking is completed also static work is completed

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index,about, contact, blog_list, product, testimonial
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,6 +8,12 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index, name="home"),
+    path("/about", about, name="about"),
+    path("/contact", contact, name="contact"),
+    path("/blog", blog_list, name="blog_list"),
+    path("/product", product, name="product"),
+    path("/testimonial", testimonial, name="testimonial"),
+    
 ]
 
 
--- ecomm/views.py
+++ ecomm/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def blog_list(request):
+    return render(request, "blog_list.html")
+
+def product(request):
+    return render(request, "product.html")
+
+def testimonial(request):
+    return render(request, "testimonial.html")
